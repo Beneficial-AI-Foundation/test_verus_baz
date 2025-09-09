6f03a25bc6af08d3052791001678768fc986ae5b
Lacramioara Astefanoaei <lacramioara.astefanoaei@gmail.com>
2025-09-09 13:50:14 +0200

    Update test_false.rs

--- src/test_false.rs
+++ src/test_false.rs
@@ -7,6 +7,18 @@ ensures 2 + 2 == 3
     assert(2 + 2 == 5);
 }
 
+fn abc()
+ensures 2 + 2 == 3
+{
+    assert(4 + 4 == 10);
+}
+
+fn def()
+ensures 2 + 2 == 7
+{
+    assert(4 + 4 == 8);
+}
+
 fn main() {}
 
 }
